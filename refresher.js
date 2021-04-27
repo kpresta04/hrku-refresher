09c1a68e07eb09c748a0584ab7d3908ff6d87d9d
Kellen Presta <kpresta04@gmail.com>
2021-04-27 13:12:21 +0100

    r

--- refresher.js
+++ refresher.js
@@ -1,8 +1,15 @@
 const fetch = require("node-fetch");
 
-function refresh() {
-	const delay = Math.floor(Math.random() * 10) + 1000;
+async function refresh() {
+	const delay = Math.floor(Math.random() * 10) + 100000;
 	console.log({ delay });
+
+	const data = await fetch(
+		"https://serene-reaches-03817.herokuapp.com/auth/refresh"
+	);
+
+	console.log(data);
+
 	setTimeout(refresh, delay);
 }
 
